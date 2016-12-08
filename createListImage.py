8f94e0521779e7e59fd24effa3e0c4d1fef45a1f
maxgreat <maxime.portaz@gmail.com>
2016-12-08 16:00:03 +0100

    first commit

---
+++ createListImage.py
@@ -0,0 +1,14 @@
+import sys
+import glob
+import os.path as path
+
+if len(sys.argv) < 3:
+	print("Usage : createList.py directory file")
+	sys.exit(1)
+
+with open(sys.argv[2], 'w') as f:
+	for file in glob.glob(path.join(sys.argv[1], '*.jpg')):
+		line = path.basename(file).split("_")
+		f.write(line[0]+'_'+line[1]+"|"+path.basename(file)+'\n')
+
+
---
+++ createListWithLabel.py
@@ -0,0 +1,13 @@
+import sys
+
+if len(sys.argv) < 3:
+	print("Usage : createListWithLabel.py listImageFile outputFile")
+	sys.exit(1)
+
+with open(sys.argv[1], 'r') as f:
+	with open(sys.argv[2], 'w') as fout :
+		for line in f:
+			stage, idx = line.split('_')[0:2]
+		fout.write(stage+'_'+idw+'|'+line)
+		
+
